a25d590ef05525e6a19612deef00feb5e0eb35e9
DadosComCafe <dadoscomcafe.dev@gmail.com>
2023-12-19 11:45:20 -0300

    ( sql/triggers/triggers.sql): Add triggers to call the functions

--- 8_apply_triggers/sql/triggers/triggers.sql
+++ 8_apply_triggers/sql/triggers/triggers.sql
@@ -1,4 +1,11 @@
+-- this trigger calls the insert_nutritional_info function when a new snack is inserted
 create trigger trigger_insert_nutritional_info
 after insert on snack
 for each row
 execute function insert_nutritional_info();
+
+-- this trigger calls the update_updatedAt_company when a record of comapny is updated
+create trigger trigger_update_company
+before update on company
+for each row
+execute function update_updatedAt_company();
