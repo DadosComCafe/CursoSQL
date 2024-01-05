7241b02e9cdc06e46eeed25c18ccc05a3d2de2cb
DadosComCafe <dadoscomcafe.dev@gmail.com>
2024-01-05 14:12:31 -0300

    (8_apply_triggers/sql/functions/functions.sql): Fix function

--- 8_apply_triggers/sql/functions/functions.sql
+++ 8_apply_triggers/sql/functions/functions.sql
@@ -1,12 +1,12 @@
 -- call this function to insert a nutritional_info 
 create or replace function insert_nutritional_info()
-returns trigger as $insert_nutritional_info_trigger$
+returns trigger as $$
 begin
 	insert into nutritional_info_normalized (id_snack, id_company)
   values (new.id, new.id_company);
   return new;
 end;
-$insert_nutritional_info_trigger$ language plpgsql;
+$$ language plpgsql;
 
 -- call this function to update the updatedAt column
 create or replace function update_updatedAt_company()
